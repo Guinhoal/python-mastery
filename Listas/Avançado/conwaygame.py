1b6d22e9fd41ff2901dfeb93496a7d87545a705b
Thiago Arthur <thiagodevprof@gmail.com>
2025-02-28 16:30:42 -0300

    Fim Listas

--- Listas/Avançado/conwaygame.py
+++ Listas/Avançado/conwaygame.py
@@ -1,30 +1,57 @@
 import random
 import time
 
-print("Iremos jogar o jogo da vida ou Conways Life")
-print("Caso queria saber mais sobre deve ver o comentário ou pesquisar")
-print("Inicialmente digite a ordem da matriz (mínimo 3, máximo 8)")
+def criar_matriz(ordem):
+    return [[random.randint(0, 1) for _ in range(ordem)] for _ in range(ordem)]
 
-ordem_matriz = int(input())
+def imprime_tabuleiro(matriz):
+    for linha in matriz:
+        print(" ".join(str(celula) for celula in linha))
+    print()
 
-if ordem_matriz < 3:
-    ordem_matriz = 3
-elif ordem_matriz > 8:
-    ordem_matriz = 8
+def contar_vizinhos(matriz, x, y):
+    ordem = len(matriz)
+    vizinhos = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),         (0, 1),
+        (1, -1), (1, 0), (1, 1)
+    ]
+    soma = 0
+    for dx, dy in vizinhos:
+        nx = (x + dx) % ordem
+        ny = (y + dy) % ordem  
+        soma += matriz[nx][ny]
+    return soma
 
-matriz = []
+def proxima_geracao(matriz):
+    ordem = len(matriz)
+    nova_matriz = [[0] * ordem for _ in range(ordem)]
+    
+    for i in range(ordem):
+        for j in range(ordem):
+            vivos = contar_vizinhos(matriz, i, j)
+            if matriz[i][j] == 1:
+                nova_matriz[i][j] = 1 if vivos in (2, 3) else 0
+            else:
+                nova_matriz[i][j] = 1 if vivos == 3 else 0
+                
+    return nova_matriz
 
-for linhasn in range(ordem_matriz):
-    linhas = []
-    for colunas in range(ordem_matriz):
-        linhas.append(random.randint(0, 1))
-    matriz.append(linhas)
+def jogo_da_vida():
+    print("Iremos jogar o Jogo da Vida de Conway!")
+    ordem = int(input("Digite a ordem da matriz (mínimo 3, máximo 8): "))
+    ordem = max(3, min(8, ordem))
 
-print("Tabuleiro Inicial:")
-print(matriz)
-time.sleep(5)
+    matriz = criar_matriz(ordem)
+    print("Tabuleiro Inicial:")
+    imprime_tabuleiro(matriz)
 
-for linhasz in range(ordem_matriz):
-    linhas1 = matriz[linhasz]
-    for colunasz in range(ordem_matriz):
-        if matriz[linhas1[ordem_matriz]]
+    while True:
+        time.sleep(1)
+        matriz = proxima_geracao(matriz)
+        imprime_tabuleiro(matriz)
+        if sum(sum(linha) for linha in matriz) == 0:
+            print("Todas as células morreram. O jogo acabou.")
+            break
+
+jogo_da_vida()
